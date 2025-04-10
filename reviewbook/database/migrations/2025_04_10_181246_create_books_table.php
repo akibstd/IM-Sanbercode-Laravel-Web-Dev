f88a23e5f127329cf5421c6a88a178a1cff9c328
akibstd <akibsupandi14@gmail.com>
2025-04-11 02:12:29 +0700

    latihanMigration

---
+++ reviewbook/database/migrations/2025_04_10_181246_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->text('content');
+            $table->string('title');
+            $table->text('summary');
+            $table->string('image');
+            $table->integer('stock');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('user');
+
+            $table->unsignedBigInteger('genre_id');
+            $table->foreign('genre_id')->references('id')->on('genres');
+         ;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
--- reviewbook/routes/web.php
+++ reviewbook/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\homeController;
 use App\Http\Controllers\daftarController;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use SebaphpstianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', [homeController::class,'home']);
 Route::get('daftar', [daftarController::class,'pendaftaran']);
